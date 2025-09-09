e405ed8199ebb10245563a64cfe866ef76607e04
shamim 200 <s40644812@gmail.com>
2025-09-09 17:00:27 +0600

    ✨ feat(footer): add newsletter subscription form
    - integrate react-bootstrap input group for email subscription
    - replace custom form with bootstrap's inputgroup and button
    - implement aos animation for enhanced user experience

--- src/Componets/Footer.jsx
+++ src/Componets/Footer.jsx
@@ -5,15 +5,17 @@ import { FaFacebookF, FaYoutube, FaLinkedinIn } from "react-icons/fa";
 
 import { FiInstagram } from "react-icons/fi";
 
+import { Button, Form, InputGroup } from "react-bootstrap";
+
 const Footer = () => {
   useEffect(() => {
-    Aos.init({ duration: 2000 });
+    Aos.init({ duration: 1000 });
   }, []);
 
   return (
     <section className="container-fluid bg-custom p-5">
       <div className="container">
-        <div className="row" data-aos="zoom-in-up">
+        <div className="row" data-aos="slide-up">
           <div className="col-lg-3 col-md-6 col-12 foot">
             <h2>Shamim BD</h2>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
@@ -21,7 +23,8 @@ const Footer = () => {
           <div className="col-lg-3 col-md-6 col-12 foot">
             <h3>Subscribe and never miss update.</h3>
 
-            <form>
+            {/* <form>
+            
               <input
                 type="email"
                 placeholder="Enter Your Email"
@@ -33,7 +36,18 @@ const Footer = () => {
                 type="submit"
                 className="btn btn-outline-secondary my-lg-3 left"
               />
-            </form>
+            </form> */}
+
+            <InputGroup className="mb-3">
+              <Form.Control placeholder="Enter Your Email" />
+              <Button
+                variant="outline-secondary"
+                className="text-capitalize"
+                type="submit"
+              >
+                submit
+              </Button>
+            </InputGroup>
           </div>
           <div className="col-lg-3 col-md-6 col-12 foot">
             <h3>Follow Me:</h3>
